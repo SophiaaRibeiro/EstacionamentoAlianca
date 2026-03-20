f7af7ef34aefa8d8da7662ffd2abbc1a3e1a3fef
Sophia Ribeiro M <126275606+SophiaaRibeiro@users.noreply.github.com>
2026-03-19 22:05:45 -0300

    Add files via upload
    att de loc e botões

--- js/script.js
+++ js/script.js
@@ -1,25 +1,45 @@
-function revealOnScroll() {
-    const elements = document.querySelectorAll(".reveal");
-
-    elements.forEach(el => {
-        const windowHeight = window.innerHeight;
-        const elementTop = el.getBoundingClientRect().top;
-
-        if (elementTop < windowHeight - 100) {
-            el.classList.add("active");
-        }
-    });
+function toggleMenu() {
+    const menu = document.querySelector("nav ul");
+    menu.classList.toggle("active");
 }
 
-window.addEventListener("scroll", revealOnScroll);
 
-window.addEventListener("load", () => {
-    const loader = document.getElementById("loader");
 
-    loader.style.opacity = "0";
+const entrada = document.getElementById("entrada");
+const saida = document.getElementById("saida");
+const veiculo = document.getElementById("veiculo");
+const total = document.getElementById("total");
 
-    setTimeout(() => {
-        loader.style.display = "none";
-    }, 500);
-});
+function calcularTotal() {
+
+    if (!entrada.value || !saida.value || !veiculo.value) {
+        total.textContent = "R$ 0,00";
+        return;
+    }
+
+    const dataEntrada = new Date(entrada.value);
+    const dataSaida = new Date(saida.value);
+
+    const diffTime = dataSaida - dataEntrada;
+    const dias = diffTime / (1000 * 60 * 60 * 24);
+
+    if (dias <= 0) {
+        total.textContent = "Data inválida";
+        return;
+    }
+
+    const diaria = Number(veiculo.value);
+    const resultado = diaria * dias;
+
+    total.textContent = `R$ ${resultado.toFixed(2)}`;
+}
+
+const hoje = new Date().toISOString().split("T")[0];
+entrada.setAttribute("min", hoje);
+saida.setAttribute("min", hoje);
+
+// eventos
+entrada.addEventListener("change", calcularTotal);
+saida.addEventListener("change", calcularTotal);
+veiculo.addEventListener("change", calcularTotal);
 
