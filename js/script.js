083c58ea85cc980bc70eba9eb41b48206abeb9a3
Sophia Ribeiro M <126275606+SophiaaRibeiro@users.noreply.github.com>
2026-03-26 11:45:13 -0300

    att finais

--- js/script.js
+++ js/script.js
@@ -43,3 +43,29 @@ entrada.addEventListener("change", calcularTotal);
 saida.addEventListener("change", calcularTotal);
 veiculo.addEventListener("change", calcularTotal);
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".menu a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop - 200) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if(link.getAttribute("href") === "#" + current){
+            link.classList.add("active");
+        }
+    });
+
+});
+
